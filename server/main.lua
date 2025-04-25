f69d3713d21f965c4bcf70754764ff88be58c593
Dominik <55330408+Dom-in@users.noreply.github.com>
2025-04-25 20:20:34 +0100

    Update 😸

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 -- MYSQL LOGIN --
-MySQL.Connect("IP", 33060, "DATABASE", "PASSWORD", "DATABASE")
+MySQL.Connect("IP", 33060, "LOGIN", "PASSWORD", "DATABASE")
 
 Events.Subscribe("ivd_identity:setIdentity", function(RocstarID, firstname, lastname, dateofbirth, sex, height)
     MySQL.Execute("UPDATE players SET firstname=?, lastname=?, dateofbirth=?, sex=?, height=? WHERE RockstarID=?", { firstname, lastname, dateofbirth, sex, height, RocstarID }, function(affectedRows)
