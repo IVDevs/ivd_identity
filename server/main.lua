e84455843ef3e0324fbc5a421eece67f88cf0b6c
Dominik <55330408+Dom-in@users.noreply.github.com>
2025-04-29 14:49:44 +0100

    Update for HMP 1.7.1

--- server/main.lua
+++ server/main.lua
@@ -1,8 +1,8 @@
 -- MYSQL LOGIN --
-MySQL.Connect("IP", 33060, "LOGIN", "PASSWORD", "DATABASE")
+Database.Connect(0, "mysqlx://user:password@host:port/database")
 
 Events.Subscribe("ivd_identity:setIdentity", function(RocstarID, firstname, lastname, dateofbirth, sex, height)
-    MySQL.Execute("UPDATE players SET firstname=?, lastname=?, dateofbirth=?, sex=?, height=? WHERE RockstarID=?", { firstname, lastname, dateofbirth, sex, height, RocstarID }, function(affectedRows)
+    Database.ExecuteAsync("UPDATE players SET firstname=?, lastname=?, dateofbirth=?, sex=?, height=? WHERE RockstarID=?", { firstname, lastname, dateofbirth, sex, height, RocstarID }, function(affectedRows)
         --Do some here when updated
     end)
 end, true)
